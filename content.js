a946e0d215c7520069515d2f24b706252f70c410
Iv <aerobinsoniv@gmail.com>
2023-01-14 21:19:12 -0500

    Send HTML of chat area to popup script

--- content.js
+++ content.js
@@ -1,5 +1,17 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(request.data);
-        sendResponse("response data from content script");
+        console.log("Popup script requested HTML");
+        
+        chatDivXpath = "/html/body/div[1]/div[1]/div[1]/main/div[1]/div/div"
+
+        let iterator = document.evaluate(chatDivXpath, document, null, XPathResult.ANY_TYPE, null);
+        let nodes = [];
+        let current = iterator.iterateNext();
+        while (current) {
+            nodes.push(current.innerHTML);
+            current = iterator.iterateNext();
+        }
+        let htmlString = nodes.join("");
+
+        sendResponse(htmlString);
     });
