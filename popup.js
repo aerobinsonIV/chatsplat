951f45bbf8a7a4df9ab4c28eb583cb1f96e0a1f0
ajay-bhagava <ajay.bhagava@gmail.com>
2023-01-14 21:11:21 -0500

    mostly finished up function to parse answer text

--- popup.js
+++ popup.js
@@ -1,36 +1,29 @@
-function uwu() {
-    alert("uwu")
-}
-
-//copy text: get answer and question text of the webpage
-function copyText () {
-    let questions = document.getElementsByClassName("min-h-[20px] flex flex-col items-start gap-4 whitespace-pre-wrap")
-    let answers = document.getElementsByClassName("markdown prose w-full break-words dark:prose-invert light")
-}
-
-function parseAnswer (answer) {
-}
-
-function questionText (questions) {
-    questionText = []
+function questionText(questions) {
+    let questionText = []
     
     for(i=0; i<questions.size; i++){
-        questionText[i] = questions[i].innerText
+        console.log(questions[i].innerText)
     }
-    return questionText
 }
 
-// gets the contents of a answer array
-function getAnswerText (answers) {
-    document.getElementsByTagName("pre")
-    
-    for(i=0; i<answer.size; i++){
-        childElements = answer[i].children
-        for(j=0; j<childElements.size; j++){
-            if(childElements[j].tagName==='PRE'){
-                extractCode(childElements[j])
+// TODO get rid of console logs, make statements that convert the HTML to markdown
+function getAnswerText(answers) {
+    for(i=0; i<answers.length; i++){
+        let childElements = answers[i].children
+        for(j=0; j<childElements.length; j++){
+            if(childElements[j].tagName === 'OL'|| childElements[j].tagName === 'UL'){
+                let listElements = childElements[j].children
+                for(k=0; k<listElements.length; k++) {
+                    console.log("- " + listElements[k].innerText)
+                }
+            } else if(childElements[j].tagName === 'PRE') {
+                // console.log(childElements[j])
+                continue
+            } else if(childElements[j].tagName === 'P') {
+                console.log(childElements[j].innerText)
+            } else {
+                console.log("how did we get here")
             }
-            
         }
     }
     
@@ -53,4 +46,13 @@ function extractCode (preHTML){
     console.log(codeDiv);
 }
 
-document.getElementById("uwuBtn").addEventListener("click", uwu)
+//copy text: get answer and question text of the webpage
+function copyText () {
+    let questions = document.getElementsByClassName("min-h-[20px] flex flex-col items-start gap-4 whitespace-pre-wrap")
+    let answers = document.getElementsByClassName("markdown prose w-full break-words dark:prose-invert light")
+    questionText(questions)
+    getAnswerText(answers)
+}
+
+document.getElementById("navChat").addEventListener("click", copyText)
+
